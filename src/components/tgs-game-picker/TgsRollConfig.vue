75f4ebd0bde3f9ca2502dcac585a0f79d8cfae4e
Nik Steinbrügge <ni851ste@htwg-konstanz.de>
2020-04-19 13:56:48 +0200

    Added TGS random game picked count with Vue Components

--- src/components/tgs-game-picker/TgsRollConfig.vue
+++ src/components/tgs-game-picker/TgsRollConfig.vue
@@ -1,52 +1,22 @@
 <template>
     <div id="tgs-picker-middle">
         <h1>middle</h1>
-        <button class="own-button-big" id="tgs-roll-games">Roll</button>
-        <div id="tgs-random-game-counter-div">
-            <button class="own-button" id="tgs-random-game-minus" v-on:click="this.minus()">-</button>
-            <div id="tgs-random-game-counter">1</div>
-            <button class="own-button" id="tgs-random-game-plus" v-on:click="plus">+</button>
-        </div>
+        <TgsRollConfigCounter/>
     </div>
 </template>
 
 <script>
+    import TgsRollConfigCounter from "./TgsRollConfigCounter";
+
     export default {
         name: "TgsRollConfig",
-        methods: {
-            plus() {
-                let currentElement = $('#tgs-random-game-counter');
-                let counter = parseInt(currentElement.text());
-                if (counter < 15) {
-                    currentElement.text(++counter);
-                }
-            },
-
-            minus() {
-                let currentElement = $('#tgs-random-game-counter');
-                let counter = parseInt(currentElement.text());
-                if (counter > 1) {
-                    currentElement.text(--counter);
-                }
-            }
-
-        }
+        components: {TgsRollConfigCounter},
     }
 
 
 </script>
 
 <style scoped>
-    #tgs-random-game-counter-div {
-        margin-top: 1em;
-        display: flex;
-        justify-content: center;
-    }
-
-    #tgs-random-game-counter {
-        padding: 5px 10px;
-    }
-
     #tgs-picker-middle {
         padding: 1em;
     }
---
+++ src/components/tgs-game-picker/TgsRollConfigCounter.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="tgs-roll-config-counter">
+        <button class="own-button-big" id="tgs-roll-games">Roll</button>
+        <div id="tgs-random-game-counter-div">
+            <button class="own-button" id="tgs-random-game-minus" v-on:click="count = decreaseCounter(count)">-</button>
+            <div id="tgs-random-game-counter"> {{ count }}</div>
+            <button class="own-button" id="tgs-random-game-plus" v-on:click="count = increaseCounter(count)">+</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TgsRollConfigCounter",
+        data() {
+            return {
+                count: 1
+            }
+        },
+        methods: {
+            increaseCounter(currCount)
+            {
+                if (currCount < 15) {
+                    return currCount + 1;
+                }
+                return currCount;
+            },
+
+            decreaseCounter(currCount)
+            {
+                if (currCount > 1) {
+                    return currCount - 1;
+                }
+                return currCount;
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    #tgs-random-game-counter-div {
+        margin-top: 1em;
+        display: flex;
+        justify-content: center;
+    }
+
+    #tgs-random-game-counter {
+        padding: 5px 10px;
+    }
+
+</style>
